807afb1518ad5e1ebc14f6ddf1f108920ab85bbc
vedat-inal <vedati@ziggo.nl>
2022-10-02 00:06:14 +0200

    Added Attachment

--- SimpleEmailApp/Models/EmailDto.cs
+++ SimpleEmailApp/Models/EmailDto.cs
@@ -5,5 +5,6 @@
         public string To { get; set; } = string.Empty;
         public string Subject { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
+        public string Attachment { get; set; } = string.Empty;
     }
 }
